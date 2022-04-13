529215320746d194cefc392b3c26de7e1951fba3
Steve Kaschimer <v-skaschimer@microsoft.com>
2022-04-13 13:54:40 -0400

    adjusting styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,10 +15,22 @@ module.exports = {
       lg: '1024px',
       xl: '1280px',
       '2xl': '1536px',
-    }
+    },
+    fontFamily: {
+      sans: ['Segoe UI','ui-sans-serif','system-ui','-apple-system','BlinkMacSystemFont','Roboto','Helvetica Neue','Arial','Noto Sans','sans-serif','Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol','Noto Color Emoji'],
+      serif: ['Merriweather', 'serif'],
+    },
   },
   variants: {
     extend: {},
+  },  
+  corePlugins: {
+    aspectRatio: false,
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
 }
