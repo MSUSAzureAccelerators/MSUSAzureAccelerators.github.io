825693a97f9b02d0d48f42a0d8a7d8f640a027b5
skaschimer <v-kaschimers@microsoft.com>
2022-05-11 13:31:56 -0400

    update styles to microsoft color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,45 @@
 module.exports = {
-  mode: 'jit',
-  content: [
-    './**/*.html'
-  ],
-  theme: {
-    extend: {
-      screens: {
-        huge: '2000px',
-      }
+    mode: 'jit',
+    content: [
+      './**/*.html'
+    ],
+    theme: {
+      extend: {
+        screens: {
+          huge: '2000px',
+        },
+        colors: {
+          'ms-orange': '#d83b01',
+          'ms-yellow': '#ffb900',
+          'ms-green': '#107c10',
+          'ms-teal': '#008575',
+          'ms-blue': '#0078d4',
+          'ms-purple': '#8661c5',
+          'ms-dark-orange': '#6b2929',
+          'ms-dark-yellow': '#6a4b16',
+          'ms-dark-green': '#054b16',
+          'ms-dark-teal': '#274b47',
+          'ms-dark-blue': '#243a5e',
+          'ms-dark-purple': '#3b2e58',
+          'ms-light-orange': '#ff9349',
+          'ms-light-yellow': '#fef000',
+          'ms-light-green': '#9bf00b',
+          'ms-light-teal': '#30e5d0',
+          'ms-light-blue': '#50e6ff',
+          'ms-light-purple': '#d59dff',
+          'ms-white': '#ffffff',
+          'ms-rich-black': '#000000',
+          'ms-extra-light-gray': '#f2f2f2',
+          'ms-light-gray': '#e6e6e6',
+          'ms-gray': '#d2d2d2',
+          'ms-mid-gray': '#737373',
+          'ms-dark-gray': '#505050',
+          'ms-extra-dark-gray': '#2f2f2f',
+          'fb-blue':'#3b5998',
+          'tw-blue': '#55acee',
+          'li-blue': '#007bb5'
+
+      },
     },
     screens: {
       xs: '375px',
